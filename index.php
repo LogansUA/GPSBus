fc8f7086b8dc6638fb6f776cb3042110e9ea2514
LogansUA <logansoleg@gmail.com>
2014-10-11 15:08:24 +0300

    add facebook slide to carousel

--- index.php
+++ index.php
@@ -64,6 +64,7 @@
         <ol class="carousel-indicators">
             <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
             <li class="" data-target="#myCarousel" data-slide-to="1"></li>
+            <li class="" data-target="#myCarousel" data-slide-to="2"></li>
         </ol>
         <div class="carousel-inner">
             <div class="item active">
@@ -84,6 +85,19 @@
                 <div class="container">
                     <div class="carousel-caption">
                         <h1>Google Play</h1>
+                        <p>Cras justo odio, dapibus ac facilisis in, egestas eget
+                            quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor
+                            id nibh ultricies vehicula ut id elit.</p>
+                        <p><a class="btn btn-lg btn-primary" href="#" role="button">Learn more</a></p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="item">
+                <img src="images/fb_logo.png" alt="Second slide">
+                <div class="container">
+                    <div class="carousel-caption">
+                        <h1>Facebook</h1>
                         <p>Cras justo odio, dapibus ac facilisis in, egestas eget
                             quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor
                             id nibh ultricies vehicula ut id elit.</p>
