7e777e65bacb9c3938cf0a54ead78462e26be0b8
LogansUA <logansoleg@gmail.com>
2014-10-20 19:22:25 +0300

    Added app_config

---
+++ assets/Config/app_config.php
@@ -0,0 +1,10 @@
+<?php
+
+function getConnectInfo() {
+    return $array = array(
+        'host' => 'localhost',
+        'user' => 'root',
+        'password' => '2g0e0l4o2004',
+        'database' => 'GPSBus'
+    );
+}
