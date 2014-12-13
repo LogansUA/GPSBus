2ef5f3897fec425eaa3fda7de164e59703a86e4f
LogansUA <logansoleg@gmail.com>
2014-12-13 17:00:02 +0200

    Added check signin form

--- views/SignIn/signin.php
+++ views/SignIn/signin.php
@@ -33,7 +33,7 @@ session_start();
                         <strong>Авторизація</strong>
                     </div>
                     <div class="panel-body">
-                        <form class="form-signin" role="form" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+                        <form class="form-signin" role="form" name="signin" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post" onsubmit="return checkForm()" >
                             <br/>
                             <div class="form-group input-group">
                                 <span class="input-group-addon"><i class="email">@</i></span>
@@ -51,6 +51,34 @@ session_start();
                             <p>Ще не зареєстровані?  <a href="../SignUp/signup.php" >Тоді реєструйтесь!</a></p>
                             <p>Не можете згадати дані? <a href="#">Відновіть їх.</a></p>
                         </form>
+
+                        <script>
+                            function checkForm() {
+                                var email = document.signin.email.value;
+                                var password = document.signin.password.value;
+
+                                var isFully = false;
+                                var isEmail = false;
+
+                                if ((email.length > 5) && (password.length > 3)) {
+                                    isFully = true;
+                                }
+
+                                for (var i = 0; i < email.length; i++) {
+                                    if (email[i] == '@') {
+                                        isEmail = true;
+                                    }
+                                }
+
+                                if (isEmail && isFully) {
+                                    return true;
+                                } else {
+                                    alert("Error!");
+                                }
+
+                                return false;
+                            }
+                        </script>
                     </div>
 
                 </div>
