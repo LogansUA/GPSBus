c3b0e2c20c15a856098550fee98fba58b19d66a4
LogansUA <logansoleg@gmail.com>
2014-12-13 16:19:58 +0200

    Change position of exit button

--- views/SignIn/signin.php
+++ views/SignIn/signin.php
@@ -7,59 +7,59 @@ session_start();
 
 <!DOCTYPE html>
 <html>
-    <head lang="en">
-        <?php include_once("../../assets/resources/Header.php"); ?>
-        <title>Авторизація</title>
-    </head>
+<head lang="en">
+    <?php include_once("../../assets/resources/Header.php"); ?>
+    <title>Авторизація</title>
+</head>
 
+<body style="padding-top: 0;">
     <div>
         <a href="../">
             <img class="exit-button" src="../images/exit.svg" alt="Exit" />
         </a>
     </div>
 
-    <body style="padding-top: 0;">
-        <div class="container">
-            <div class="row text-center pad-top ">
-                <div class="col-md-12">
-                    <h2><a href="<?php echo $_SERVER['PHP_SELF']; ?>" class="headText">Сторінка авторизації</a></h2>
-                </div>
-            </div>
-
-            <div class="row  pad-top">
-                <div class="col-md-4 col-md-offset-4 col-sm-6 col-sm-offset-3 col-xs-10 col-xs-offset-1">
-                    <div class="panel panel-default">
-                        <div class="panel-heading">
-                            <strong>Авторизація</strong>
-                        </div>
-                        <div class="panel-body">
-                            <form class="form-signin" role="form" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
-                                <br/>
-                                <div class="form-group input-group">
-                                    <span class="input-group-addon"><i class="email">@</i></span>
-                                    <input type="text" class="form-control" autofocus="" placeholder="Email" name="email" />
-                                </div>
-                                <div class="form-group input-group">
-                                    <span class="input-group-addon"><i class="fa fa-lock lock"></i></span>
-                                    <input type="password" class="form-control" placeholder="Пароль" name="password" />
-                                </div>
-                                <?php
-                                include_once("../../controllers/SignInController.php");
-                                ?>
-                                <button class="btn btn-lg btn-success btn-block" type="submit" name="sign_in" value="#">Авторизуватися</button>
-                                <hr />
-                                <p>Ще не зареєстровані?  <a href="../SignUp/signup.php" >Тоді реєструйтесь!</a></p>
-                                <p>Не можете згадати дані? <a href="#">Відновіть їх.</a></p>
-                            </form>
-                        </div>
-
-                    </div>
-                </div>
+    <div class="container">
+        <div class="row text-center pad-top ">
+            <div class="col-md-12">
+                <h2><a href="<?php echo $_SERVER['PHP_SELF']; ?>" class="headText">Сторінка авторизації</a></h2>
             </div>
         </div>
 
-        <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-        <script src="../js/ie10-viewport-bug-workaround.js"></script>
+        <div class="row  pad-top">
+            <div class="col-md-4 col-md-offset-4 col-sm-6 col-sm-offset-3 col-xs-10 col-xs-offset-1">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <strong>Авторизація</strong>
+                    </div>
+                    <div class="panel-body">
+                        <form class="form-signin" role="form" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+                            <br/>
+                            <div class="form-group input-group">
+                                <span class="input-group-addon"><i class="email">@</i></span>
+                                <input type="text" class="form-control" autofocus="" placeholder="Email" name="email" />
+                            </div>
+                            <div class="form-group input-group">
+                                <span class="input-group-addon"><i class="fa fa-lock lock"></i></span>
+                                <input type="password" class="form-control" placeholder="Пароль" name="password" />
+                            </div>
+                            <?php
+                            include_once("../../controllers/SignInController.php");
+                            ?>
+                            <button class="btn btn-lg btn-success btn-block" type="submit" name="sign_in" value="#">Авторизуватися</button>
+                            <hr />
+                            <p>Ще не зареєстровані?  <a href="../SignUp/signup.php" >Тоді реєструйтесь!</a></p>
+                            <p>Не можете згадати дані? <a href="#">Відновіть їх.</a></p>
+                        </form>
+                    </div>
 
-    </body>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="../js/ie10-viewport-bug-workaround.js"></script>
+
+</body>
 </html>
--- views/SignUp/signup.php
+++ views/SignUp/signup.php
@@ -4,108 +4,108 @@ include_once("../../assets/redirect.php");
 ?>
 <!DOCTYPE html>
 <html>
-    <head lang="en">
-        <?php include_once("../../assets/resources/Header.php"); ?>
-        <title>Реєстрація</title>
-    </head>
+<head lang="en">
+    <?php include_once("../../assets/resources/Header.php"); ?>
+    <title>Реєстрація</title>
+</head>
 
+<body style="padding-top: 0;">
     <div>
         <a href="../">
             <img class="exit-button" src="../images/exit.svg" alt="Exit" />
         </a>
     </div>
 
-    <body style="padding-top: 0;">
-        <div class="container">
-            <div class="row text-center pad-top">
-                <div class="col-md-12">
-                    <h2><a href="<?php echo $_SERVER['PHP_SELF']; ?>" class="headText">Сторінка реєстрації</a></h2>
-                </div>
+    <div class="container">
+        <div class="row text-center pad-top">
+            <div class="col-md-12">
+                <h2><a href="<?php echo $_SERVER['PHP_SELF']; ?>" class="headText">Сторінка реєстрації</a></h2>
             </div>
+        </div>
 
-            <div class="row  pad-top">
-                <div class="col-md-4 col-md-offset-4 col-sm-6 col-sm-offset-3 col-xs-10 col-xs-offset-1">
-                    <div class="panel panel-default">
-                        <div class="panel-heading">
-                            <strong>Реєстрація</strong>
-                        </div>
-                        <div class="panel-body">
-                            <form class="form-signin" role="form" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
-                                <br/>
-                                <div class="form-group input-group">
-                                    <span class="input-group-addon"><i class="fa fa-circle-o-notch name"  ></i></span>
-                                    <input type="text" class="form-control" placeholder="Ім’я" name="firstName" />
-                                </div>
-                                <div class="form-group input-group">
-                                    <span class="input-group-addon"><i class="fa fa-tag tag"  ></i></span>
-                                    <input type="text" class="form-control" placeholder="Нікнейм" name="nickName" />
-                                </div>
-                                <div class="form-group input-group">
-                                    <span class="input-group-addon"><i class="email">@</i></span>
-                                    <input type="text" class="form-control" placeholder="Email" name="email" />
-                                </div>
-                                <div class="form-group input-group">
-                                    <span class="input-group-addon"><i class="fa fa-lock lock"  ></i></span>
-                                    <input type="password" class="form-control" placeholder="Пароль" name="password" />
-                                </div>
-                                <div class="form-group input-group">
-                                    <span class="input-group-addon"><i class="fa fa-lock lock"  ></i></span>
-                                    <input type="password" class="form-control" placeholder="Повторіть пароль" name="rePassword" />
-                                </div>
+        <div class="row  pad-top">
+            <div class="col-md-4 col-md-offset-4 col-sm-6 col-sm-offset-3 col-xs-10 col-xs-offset-1">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <strong>Реєстрація</strong>
+                    </div>
+                    <div class="panel-body">
+                        <form class="form-signin" role="form" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+                            <br/>
+                            <div class="form-group input-group">
+                                <span class="input-group-addon"><i class="fa fa-circle-o-notch name"  ></i></span>
+                                <input type="text" class="form-control" placeholder="Ім’я" name="firstName" />
+                            </div>
+                            <div class="form-group input-group">
+                                <span class="input-group-addon"><i class="fa fa-tag tag"  ></i></span>
+                                <input type="text" class="form-control" placeholder="Нікнейм" name="nickName" />
+                            </div>
+                            <div class="form-group input-group">
+                                <span class="input-group-addon"><i class="email">@</i></span>
+                                <input type="text" class="form-control" placeholder="Email" name="email" />
+                            </div>
+                            <div class="form-group input-group">
+                                <span class="input-group-addon"><i class="fa fa-lock lock"  ></i></span>
+                                <input type="password" class="form-control" placeholder="Пароль" name="password" />
+                            </div>
+                            <div class="form-group input-group">
+                                <span class="input-group-addon"><i class="fa fa-lock lock"  ></i></span>
+                                <input type="password" class="form-control" placeholder="Повторіть пароль" name="rePassword" />
+                            </div>
 
-                                <?php
-                                if (isset($_POST['sign_up'])) {
-                                    $firstName = $_POST['firstName'];
-                                    $nickName = $_POST['nickName'];
-                                    $email = $_POST['email'];
-                                    $password = $_POST['password'];
-                                    $rePassword = $_POST['rePassword'];
+                            <?php
+                            if (isset($_POST['sign_up'])) {
+                                $firstName = $_POST['firstName'];
+                                $nickName = $_POST['nickName'];
+                                $email = $_POST['email'];
+                                $password = $_POST['password'];
+                                $rePassword = $_POST['rePassword'];
 
-                                    $database = new DataBaseClass();
-                                    $messages = new MessagesClass();
-                                    $check = new CheckClass();
-                                    $driver = new DriverClass();
+                                $database = new DataBaseClass();
+                                $messages = new MessagesClass();
+                                $check = new CheckClass();
+                                $driver = new DriverClass();
 
-                                    if (!empty($firstName) &
-                                        !empty($nickName)  &
-                                        !empty($email)     &
-                                        !empty($password)  &
-                                        !empty($rePassword)) {
+                                if (!empty($firstName) &
+                                    !empty($nickName)  &
+                                    !empty($email)     &
+                                    !empty($password)  &
+                                    !empty($rePassword)) {
 
-                                        if ($check->isNewUser($email, $driver->getEmail())) {
+                                    if ($check->isNewUser($email, $driver->getEmail())) {
 
-                                            if ($password == $rePassword) {
-                                                $database->insertData("INSERT INTO `Driver`(`firstName`, `nickName`, `email`, `password`, `group`)
-                                                                   VALUES ('$firstName', '$nickName', '$email', '$password', 'users')");
-                                                htmlRedirect("signin.php");
-                                            } else {
-                                                $messages->getErrorBar("Паролі повинні співпадати.");
-                                            }
+                                        if ($password == $rePassword) {
+                                            $database->insertData("INSERT INTO `Driver`(`firstName`, `nickName`, `email`, `password`, `group`)
+                                                               VALUES ('$firstName', '$nickName', '$email', '$password', 'users')");
+                                            htmlRedirect("signin.php");
                                         } else {
-                                            $messages->getErrorBar("Email $email вже зареєстрований.");
+                                            $messages->getErrorBar("Паролі повинні співпадати.");
                                         }
-
                                     } else {
-                                        $messages->getErrorBar("Ви ввели не всі дані.");
+                                        $messages->getErrorBar("Email $email вже зареєстрований.");
                                     }
+
+                                } else {
+                                    $messages->getErrorBar("Ви ввели не всі дані.");
                                 }
-                                ?>
+                            }
+                            ?>
 
-                                <button class="btn btn-lg btn-primary btn-block" type="submit" name="sign_up" value="#">Зареєструватися</button>
+                            <button class="btn btn-lg btn-primary btn-block" type="submit" name="sign_up" value="#">Зареєструватися</button>
 
-                                <hr />
-                                Вже заєстровані? <a href="../SignIn/signin.php">Тоді заходьте!</a>
-                            </form>
-                        </div>
+                            <hr />
+                            Вже заєстровані? <a href="../SignIn/signin.php">Тоді заходьте!</a>
+                        </form>
                     </div>
                 </div>
             </div>
         </div>
+    </div>
 
-        <!-- JAVASCRIPT FILES PLACED AT THE BOTTOM TO REDUCE THE LOADING TIME  -->
-        <script src="../js/jquery-1.10.2.js"></script>
-        <!-- BOOTSTRAP SCRIPTS  -->
-        <script src="../js/bootstrap.js"></script>
+    <!-- JAVASCRIPT FILES PLACED AT THE BOTTOM TO REDUCE THE LOADING TIME  -->
+    <script src="../js/jquery-1.10.2.js"></script>
+    <!-- BOOTSTRAP SCRIPTS  -->
+    <script src="../js/bootstrap.js"></script>
 
-    </body>
+</body>
 </html>
