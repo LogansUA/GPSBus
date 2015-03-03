de16ba360c32a8515783dd2090fff274ac746ff3
LogansUA <logansoleg@gmail.com>
2015-03-03 12:51:41 +0200

    Add gender field for Entity

--- ProfileController.php
+++ ProfileController.php
@@ -2,20 +2,31 @@
 
 include_once('bootstrap.php');
 
-if (isset($_POST["save"])) {
+if (isset($_POST['save'])) {
     $idDriver  = $_SESSION['idDriver'];
     $firstName = $_POST['firstName'];
     $lastName  = $_POST['lastName'];
+    $gender    = $_POST['gender'];
 
     $_SESSION['firstName'] = $firstName;
     $_SESSION['lastName']  = $lastName;
+
+    switch($gender) {
+        case 'male':
+            $_SESSION['gender'] = 'Чоловіча';
+            break;
+        case 'female':
+            $_SESSION['gender'] = 'Жіноча';
+            break;
+    }
 
     $driver = $entityManager->getRepository('Driver')->findOneBy([
         'id' => $idDriver,
     ]);
 
     $driver->setFirstName($firstName)
-        ->setLastName($lastName);
+        ->setLastName($lastName)
+        ->setGender($gender);
 
     $entityManager->flush();
 
