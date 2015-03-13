222ca278dc9ab21957b6041df15ee81da4f89603
LogansUA <logansoleg@gmail.com>
2015-03-13 10:38:16 +0200

    Add "admin panel" button

--- views/Admin/index.php
+++ views/Admin/index.php
@@ -9,15 +9,15 @@ session_start();
     <head lang="en">
         <?php include_once("../../assets/resources/Header.php"); ?>
 
-        <link rel="stylesheet" href="http://cdn.leafletjs.com/leaflet-0.7.3/leaflet.css" />
+<!--        <link rel="stylesheet" href="http://cdn.leafletjs.com/leaflet-0.7.3/leaflet.css" />-->
         <title>Головна сторінка</title>
     </head>
 
     <body>
         <?php include_once("../../assets/resources/NavigationPanel.php"); ?>
 
-        <div style="margin: 2% 2% 0 0;">
-            <div class="well bs-component" style="float: left; margin-left: 2%; width: 32%">
+        <div class="main-content">
+            <div class="well bs-component" style="float: left; margin-left: 2%; width: 30%">
                 <form class="form-horizontal" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
                     <fieldset>
                         <legend><strong>Створення маршруту</strong></legend>
@@ -56,19 +56,26 @@ session_start();
                 </form>
             </div>
 
-            <div id="map" style="float: right;"></div>
+            <div id="map" style="float: right; margin: 0 2% 0 0"></div>
         </div>
 
         <footer>
             <?php include_once("../../assets/resources/Footer.php"); ?>
         </footer>
 
-        <script src="http://cdn.leafletjs.com/leaflet-0.7.3/leaflet.js"></script>
+<!--        <script src="http://cdn.leafletjs.com/leaflet-0.7.3/leaflet.js"></script>-->
+<!---->
+<!--        <script src="http://www.mapquestapi.com/sdk/leaflet/v1.s/mq-map.js?key=Kmjtd%7Cluua2qu7n9%2C7a%3Do5-lzbgq"></script>-->
+<!--        <script src="http://www.mapquestapi.com/sdk/leaflet/v1.s/mq-routing.js?key=Kmjtd%7Cluua2qu7n9%2C7a%3Do5-lzbgq"></script>-->
+<!---->
+<!--        <script src="../../web/js/jquery.js"></script>-->
+<!--        <script src="../../web/js/map_admin.js"></script>-->
 
-        <script src="http://www.mapquestapi.com/sdk/leaflet/v1.s/mq-map.js?key=Kmjtd%7Cluua2qu7n9%2C7a%3Do5-lzbgq"></script>
-        <script src="http://www.mapquestapi.com/sdk/leaflet/v1.s/mq-routing.js?key=Kmjtd%7Cluua2qu7n9%2C7a%3Do5-lzbgq"></script>
-
-        <script src="../../web/js/jquery.js"></script>
-        <script src="../../web/js/map_admin.js"></script>
+        <script>
+            $(document).ready(function() {
+                $('#main-button').removeClass('active');
+                $('#admin-panel-button').addClass('active');
+            });
+        </script>
     </body>
 </html>
