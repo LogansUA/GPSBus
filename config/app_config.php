3e90122d98763463506d63b48f3c930c0babda31
Oleg Kachinsky <Logansoleg@gmail.com>
2015-10-11 20:29:32 +0300

    Update app_config.php

--- config/app_config.php
+++ config/app_config.php
@@ -4,7 +4,7 @@ function getConnectInfo() {
     return array(
         'host' => 'localhost',
         'user' => 'root',
-        'password' => '2g0e0l4o2004',
-        'database' => 'GPS Bus'
+        'password' => '',
+        'database' => 'gps_bus'
     );
 }
